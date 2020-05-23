8cee883f66893b760b2dcf6a95484c9909dff3e5
Kim S <ksemenza@gmail.com>
2020-05-23 11:07:55 -0700

    setup client side

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Tag",
+    embedded: false
+  },
+  {
+    name: "Project",
+    embedded: false
+  },
+  {
+    name: "ProjectTagElement",
+    embedded: false
+  },
+  {
+    name: "Note",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:9000`
+});
+exports.prisma = new exports.Prisma();
